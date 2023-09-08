562af2b79ca5407fc7fc6665ca68271873024753
William <rscott.sites@gmail.com>
2023-09-08 09:12:51 -0700

    saving files

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,80 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Ryan');
+// console.log(23);
+
+// let country = 'United States';
+// let continent = 'North America';
+// let population = 340298334;
+
+// console.log('Country: ' + country + ' ' + 'Continent: ' + continent + ' ' + 'Population' + ' ' + population)
+
+// let massMark, heightMark, massJohn, heightJohn;
+// let BMIJohn, BMIMark;
+// let markHigherBMI = false;
+
+// massMark = 78;
+// heightMark = 1.69;
+// massJohn = 92;
+// heightJohn = 1.95;
+
+// BMIJohn = massJohn / (heightJohn * heightJohn);
+// BMIMark = massMark / (heightMark * heightMark);
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//     markHigherBMI = true;
+//     console.log(markHigherBMI);
+// }
+
+
+// const age = 15;
+
+// if (age >= 18) {
+//     console.log('Sarah can start driving license 🚗')
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 2012;
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century);
+
+// const day = 'thursday';
+
+// switch(day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples')
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     default:
+//         console.log('Not valid day');
+//         break;
+// }
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💦');
